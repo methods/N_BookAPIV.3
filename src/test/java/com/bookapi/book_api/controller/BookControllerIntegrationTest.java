ad0e15d487b126f0b244ac7f3aa17aac5d2f9934
Nicholas.Goh@methods.co.uk <Nicholas.Goh@methods.co.uk>
2025-09-05 13:57:28 +0100

    test(books): Add failing integration test for GET /books/{id} happy path

--- src/test/java/com/bookapi/book_api/controller/BookControllerIntegrationTest.java
+++ src/test/java/com/bookapi/book_api/controller/BookControllerIntegrationTest.java
@@ -1,16 +1,22 @@
 // src/test/java/com/bookapi/bookapi/controller/BookControllerIntegrationTest.java
 package com.bookapi.book_api.controller;
 
+import com.bookapi.book_api.model.Book;
+import com.bookapi.book_api.repository.BookRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest // (1)
@@ -19,6 +25,14 @@ public class BookControllerIntegrationTest {
 
     @Autowired // (3)
     private MockMvc mockMvc;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @AfterEach
+    void tearDown() {
+        bookRepository.deleteAll();
+    }
 
     @Test // (4)
     @WithMockUser
@@ -30,5 +44,25 @@ public class BookControllerIntegrationTest {
         // Act & Assert
         mockMvc.perform(get("/books/{bookId}", bookId)) // (5)
                 .andExpect(status().isNotImplemented()); // (6)
+    }
+
+    @Test
+    @WithMockUser
+    void getBookById_whenBookExists_shouldReturnBookDetails() throws Exception {
+        // Arrange
+        // Create a book object, save it to the database
+        Book testBook = new Book("The Hobbit", "J.R.R.Tolkien", "An Unexpected Journey");
+        bookRepository.save(testBook);
+
+        // Act
+        // Make a GET request to the endpoint using the new book's id
+        mockMvc.perform(get("/books/{bookId}", testBook.getId()))
+                // Assert
+                // Expect HTTP 200 OK Status code
+                .andExpect(status().isOk())
+                // Expect the response JSON to contain the supplied title and author
+                .andExpect(jsonPath("$.title", is("The Hobbit")))
+                .andExpect(jsonPath("$.author", is("J.R.R.Tolkien")));
+
     }
 }
